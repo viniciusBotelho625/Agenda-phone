391dcbc1c56f82ba34ab10b7b80bd86414967fe1
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2021-04-02 17:09:54 -0300

    componente contato

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,18 +1,25 @@
+import 'package:app_crud/components/user.tile.dart';
 import 'package:app_crud/data/dummy_users.dart';
 import 'package:flutter/material.dart';
 
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const users = {...Dummy_users};
+    final users = {...Dummy_users};
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de usuários'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: ListView.builder(
           itemCount: users.length,
-          itemBuilder: (ctx, i) => Text(users.values.elementAt(i).name)),
+          itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i))),
     );
   }
 }
